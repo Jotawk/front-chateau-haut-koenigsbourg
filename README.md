7cd5b3b6e406b8cb40bbf69b5d65dc8f32e95ada
Jonathan Tawk <jonathan.tawk@live.fr>
2023-02-23 17:08:10 +0100

    Publication du projet

---
+++ README.md
@@ -0,0 +1,2 @@
+# front-chateau-haut-koenigsbourg
+![Page d'accueil du site](https://zupimages.net/up/23/08/l9g2.jpg)
---
+++ main.js
@@ -0,0 +1,38 @@
+const sr = ScrollReveal({
+  reset: false
+});
+
+
+// HERO.CONTENT
+ScrollReveal().reveal('.headline', { 
+  delay: 250,
+  duration: 1650
+});
+
+ScrollReveal().reveal('.subline', { 
+  delay: 450,
+  duration: 1650,
+  distance: '10%'
+});
+
+ScrollReveal().reveal('button', { 
+  delay: 700,
+  duration: 2100,
+  distance: '30%'
+});
+
+
+// LOCATIONS.CONTENT
+ScrollReveal().reveal('.location-content', { 
+  delay: 300,
+  duration: 1650,
+  distance: '10%'
+});
+
+// CARDS.CONTENT
+ScrollReveal().reveal('.card', { 
+  delay: 300,
+  duration: 1650,
+  distance: '10%'
+});
+
